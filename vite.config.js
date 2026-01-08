8e3dc4ea8e4a28262314bc771b76e0323e6ba706
edward920727 <c9grcsyp82@privaterelay.appleid.com>
2026-01-08 23:01:11 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  base: '/12/', // 這裡填入你的倉庫名稱，前後都要斜線
+  // ... 其他原本的設定
+}
