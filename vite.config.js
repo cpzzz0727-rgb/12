83813c27b3d5a5a5732da269a406ef65c05466c3
edward920727 <c9grcsyp82@privaterelay.appleid.com>
2026-01-09 12:58:07 +0800

    12

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/12/',
+  // 開發環境使用 '/'，生產環境使用 '/12/'
+  base: process.env.NODE_ENV === 'production' ? '/12/' : '/',
   build: {
     // 確保 404.html 也被複製到 dist
     copyPublicDir: true,
