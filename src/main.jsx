792adcfc846084eecca592f39b4270b66bd08bbc
edward920727 <c9grcsyp82@privaterelay.appleid.com>
2026-01-09 13:41:07 +0800

    3

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,13 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+// 根據環境動態設置 basename
+// 開發環境使用 '/'，生產環境（GitHub Pages）使用 '/12/'
+const basename = import.meta.env.PROD ? '/12/' : '/'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/12/">
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
